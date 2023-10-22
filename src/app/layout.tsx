1ea02ddaa737bb06decb1f7bd4029f0996bf9676
Johannes Naylor <jonaylor89@gmail.com>
2023-10-21 19:30:05 -0500

    og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,11 @@ import { Rubik } from 'next/font/google'
 
 const rubik = Rubik({ subsets: ['latin'] })
 
+const title = 'Tapped.ai - Viral Checker'
+const description = 'check if your song is viral on tiktok'
 export const metadata: Metadata = {
-  title: 'Viral Checker',
-  description: 'check if your song is viral on tiktok',
+  title,
+  description,
 }
 
 export default function RootLayout({
@@ -17,6 +19,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <meta
+          name="description"
+          content={description}
+        />
+        <meta property="og:site_name" content="tapped.ai" />
+        <meta
+          property="og:description"
+          content={description}
+        />
+        <meta
+          property="og:title"
+          content={title}
+        />
+        <meta property="og:image" content="https://getmusicviralchecker.com/og.png"></meta>
+        <meta property="og:url" content="https://tapped.ai"></meta>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content={title}
+        />
+        <meta
+          name="twitter:description"
+          content={description}
+        />
+        <meta property="twitter:image" content="https://getmusicviralchecker.com/og.png"></meta>
+      </head>
       <body className={rubik.className}>
         {children}
         <Analytics />
